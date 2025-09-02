3dda47820a5de8f71aadefc2298d8ebb26a2bae6
thierryLchaves <thierry.lucaschaves@gmail.com>
2025-09-01 23:57:56 -0300

    módulo 4, sessão 5 finalizada.

--- 04-Programação Orientada a Objetos com Python/5-Interfaces e Classes Abstratas com Python/src/01_atributos_classe_instnacia.py
+++ 04-Programação Orientada a Objetos com Python/5-Interfaces e Classes Abstratas com Python/src/01_atributos_classe_instnacia.py
@@ -3,21 +3,22 @@ class Estudate:
 
     def __init__(self, nome, numero):
         self.nome = nome
-        self.numero = numero 
+        self.numero = numero
 
     def __str__(self):
         return f"{self.nome} ({self.numero}) - {self.escola}"
-    
+
+
 def mostrar_valores(*objs):
     for obj in objs:
         print(obj)
 
 
-aluno_1 = Estudate("Thierry",1)
-aluno_2 = Estudate("Tyara",2)
-mostrar_valores(aluno_1,aluno_2)
+aluno_1 = Estudate("Thierry", 1)
+aluno_2 = Estudate("Tyara", 2)
+mostrar_valores(aluno_1, aluno_2)
 
 
 Estudate.escola = "Python"
 aluno_3 = Estudate("Larissa", 3)
-mostrar_valores(aluno_1,aluno_2,aluno_3)
+mostrar_valores(aluno_1, aluno_2, aluno_3)
